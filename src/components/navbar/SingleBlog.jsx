f7ce03d2afd99030c7981bf682ca76ba0613de6b
Mncconcepts <clintonnweze111@gmail.com>
2025-03-18 17:25:03 +0100

    added app

--- src/components/navbar/SingleBlog.jsx
+++ src/components/navbar/SingleBlog.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./SingleBlog.css";
 
 const SingleBlog = () => {
   const location = useLocation();
   const post = location.state?.post || JSON.parse(localStorage.getItem("selectedBlog"));
+
+  useEffect(() => {
+    if (location.state?.post) {
+      // Save to localStorage when arriving from Blog.jsx
+      localStorage.setItem("selectedBlog", JSON.stringify(location.state.post));
+    }
+  }, [location.state]);
 
   if (!post) {
     return <p className="error-message">Blog post not found!</p>;
@@ -18,18 +25,17 @@ const SingleBlog = () => {
         <span>{post.date}</span> | <span>{post.category}</span>
       </p>
 
-      {/* Display all post details dynamically */}
       <div className="blog-details">
-        {Object.entries(post).map(([key, value], index) => (
-          key !== "image" && ( // Exclude image from mapping
+        {Object.entries(post).map(([key, value], index) =>
+          key !== "image" ? (
             <p key={index} className="blog-text">
               <strong>{key.replace(/_/g, " ").toUpperCase()}:</strong> {value}
             </p>
-          )
-        ))}
+          ) : null
+        )}
       </div>
       <Link to="/blog">
-      <button className="btn btn-success" type="submit">Back to Blogpage</button>
+        <button className="btn btn-success">Back to Blogpage</button>
       </Link>
     </div>
   );
