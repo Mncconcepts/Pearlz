f84deb0cd2576a131fdae15a00b1025323eac359
Mncconcepts <clintonnweze111@gmail.com>
2025-03-26 14:24:43 +0100

    new

--- src/main.jsx
+++ src/main.jsx
@@ -32,7 +32,7 @@ import Shop from './components/navbar/Shop.jsx';
 import Signup from './components/navbar/Signup.jsx';
 import Help from './components/navbar/Help.jsx';
 import Terms from './components/navbar/Terms.jsx';
-import Checkout from './components/navbar/Checkout.jsx';
+import Checkout from "./components/navbar/Checkout.jsx";
 
 
 
